bff54acf11b0f2246a1d5c12e577fc16c903b55b
Gustavo Rodriguez Baldera <gbaldera@gmail.com>
2012-10-02 17:10:55 -0400

    Translating form messages to spanish

--- todo/forms.py
+++ todo/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form, TextField, PasswordField, Required, Email
 
 class LoginForm(Form):
-    email = TextField('Email', [Required(message='Este campo es requerido'), Email(message='Este campo no es un e-mail valido')])
+    email = TextField('Email', [Required(message='Este campo es requerido'), Email(message='E-mail no valido')])
     password = PasswordField('Password', [Required(message='Este campo es requerido')])
